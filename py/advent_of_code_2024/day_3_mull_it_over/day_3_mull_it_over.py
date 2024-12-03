108304794312983fba837ba5a122b7884f548e14
dbartok <34172998+dbartok@users.noreply.github.com>
2024-12-03 22:14:17 +0000

    Add Day 3: Mull It Over (part 2)

--- py/advent_of_code_2024/day_3_mull_it_over/day_3_mull_it_over.py
+++ py/advent_of_code_2024/day_3_mull_it_over/day_3_mull_it_over.py
@@ -10,9 +10,9 @@ def solve_part_one(puzzle_input):
     :param puzzle_input: The input data as string
     :return: Solution for part one
     """
-    mul_expression_pattern = r'mul\((\d+),(\d+)\)'
-    mul_expression_tuples = [(int(x), int(y)) for x, y in re.findall(mul_expression_pattern, puzzle_input)]
-    return sum(x * y for x, y in mul_expression_tuples)
+    instructions = parse_puzzle_input(puzzle_input)
+    filtered_instructions = [instruction for instruction in instructions if instruction[0] == 'mul']
+    return create_sum_from_instructions(filtered_instructions)
 
 
 def solve_part_two(puzzle_input):
@@ -22,7 +22,36 @@ def solve_part_two(puzzle_input):
     :param puzzle_input: The input data as string
     :return: Solution for part two
     """
-    pass
+    instructions = parse_puzzle_input(puzzle_input)
+    return create_sum_from_instructions(instructions)
+
+
+def parse_puzzle_input(puzzle_input):
+    pattern = r'mul\((\d+),(\d+)\)|do\(\)|don\'t\(\)'
+    instructions = []
+
+    for match in re.finditer(pattern, puzzle_input):
+        if match.group(1) is not None and match.group(2) is not None:
+            instructions.append(('mul', int(match.group(1)), int(match.group(2))))
+        else:
+            instructions.append((match.group(0),))
+
+    return instructions
+
+
+def create_sum_from_instructions(instructions):
+    total_sum = 0
+    is_summing_enabled = True
+
+    for instruction in instructions:
+        if instruction[0] == 'mul' and is_summing_enabled:
+            total_sum += instruction[1] * instruction[2]
+        elif instruction[0] == 'do()':
+            is_summing_enabled = True
+        elif instruction[0] == "don't()":
+            is_summing_enabled = False
+
+    return total_sum
 
 
 def main():
@@ -45,7 +74,11 @@ class TestAdventOfCode(unittest.TestCase):
         self.assertEqual(solve_part_one(puzzle_input), expected_output)
 
     def test_part_two(self):
-        pass
+        puzzle_input = textwrap.dedent("""
+            xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
+        """).strip()
+        expected_output = 48
+        self.assertEqual(solve_part_two(puzzle_input), expected_output)
 
 
 if __name__ == "__main__":
