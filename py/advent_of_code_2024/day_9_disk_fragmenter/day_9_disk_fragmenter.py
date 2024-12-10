17be119d76c1af1bcf511f8fd5540ff8640a33fa
dbartok <34172998+dbartok@users.noreply.github.com>
2024-12-10 00:18:15 +0000

    Add Day 9: Disk Fragmenter (part 2)

--- py/advent_of_code_2024/day_9_disk_fragmenter/day_9_disk_fragmenter.py
+++ py/advent_of_code_2024/day_9_disk_fragmenter/day_9_disk_fragmenter.py
@@ -21,56 +21,58 @@ def solve_part_two(puzzle_input):
     :param puzzle_input: The input data as string
     :return: Solution for part two
     """
-    pass
+    compactor = WholeFileDiskCompactor(puzzle_input)
+    compactor.compact()
+    return compactor.get_checksum()
 
 
 class DiskCompactor:
     def __init__(self, disk_map: str):
-        self.disk_map = disk_map
-        self.file_sizes, self.space_sizes = self._parse_disk_map()
+        self._disk_map = disk_map
+        self._file_sizes, self._space_sizes = self._parse_disk_map()
 
         # List to store (file_id, file_size) tuples
-        self.new_disk_layout = []
+        self._new_disk_layout = []
 
     def compact(self):
-        self.new_disk_layout.clear()
+        self._new_disk_layout.clear()
 
         # Start by placing the first file
-        self.new_disk_layout.append((0, self.file_sizes[0]))
+        self._new_disk_layout.append((0, self._file_sizes[0]))
 
-        file_to_move_index = len(self.file_sizes) - 1
-        remaining_file_size = self.file_sizes[file_to_move_index]
+        file_to_move_index = len(self._file_sizes) - 1
+        remaining_file_size = self._file_sizes[file_to_move_index]
         space_index = 0
-        remaining_space_size = self.space_sizes[space_index]
+        remaining_space_size = self._space_sizes[space_index]
         while space_index < file_to_move_index - 1:
             if remaining_space_size == 0:
                 # Write file that was originally after this space
                 file_after_space_index = space_index + 1
-                self.new_disk_layout.append((file_after_space_index, self.file_sizes[file_after_space_index]))
+                self._new_disk_layout.append((file_after_space_index, self._file_sizes[file_after_space_index]))
 
                 # Move onto next space
                 space_index += 1
-                remaining_space_size = self.space_sizes[space_index]
+                remaining_space_size = self._space_sizes[space_index]
 
             # Write the file chunk to the current space
             write_size = min(remaining_file_size, remaining_space_size)
-            self.new_disk_layout.append((file_to_move_index, write_size))
+            self._new_disk_layout.append((file_to_move_index, write_size))
             remaining_file_size -= write_size
             remaining_space_size -= write_size
 
             if remaining_file_size == 0:
                 # Move onto previous file (we process the files backwards)
                 file_to_move_index -= 1
-                remaining_file_size = self.file_sizes[file_to_move_index]
+                remaining_file_size = self._file_sizes[file_to_move_index]
 
         # Write final chunk of last file
-        self.new_disk_layout.append((file_to_move_index, remaining_file_size))
+        self._new_disk_layout.append((file_to_move_index, remaining_file_size))
 
     def get_checksum(self):
         expanded_layout = []
 
         # Expand the new disk layout to get actual blocks
-        for file_id, file_size in self.new_disk_layout:
+        for file_id, file_size in self._new_disk_layout:
             expanded_layout.extend([file_id] * file_size)
 
         # Calculate checksum by summing the position * file_id for each block
@@ -81,13 +83,50 @@ class DiskCompactor:
         file_sizes = []
         space_sizes = []
 
-        for i in range(len(self.disk_map)):
+        for i in range(len(self._disk_map)):
             if i % 2 == 0:  # Even indices (0, 2, 4, ...) are file sizes
-                file_sizes.append(int(self.disk_map[i]))
+                file_sizes.append(int(self._disk_map[i]))
             else:  # Odd indices (1, 3, 5, ...) are space sizes
-                space_sizes.append(int(self.disk_map[i]))
+                space_sizes.append(int(self._disk_map[i]))
+
+        # Add extra 0 space at the end as padding
+        space_sizes.append(0)
 
         return file_sizes, space_sizes
+
+
+class WholeFileDiskCompactor(DiskCompactor):
+    def compact(self):
+        self._new_disk_layout.clear()
+
+        file_id_order = list(range(0, len(self._file_sizes)))
+
+        # Iterate files in reverse order (starting with the highest file ID)
+        for file_index in range(len(self._file_sizes) - 1, -1, -1):
+            file_size = self._file_sizes[file_index]
+
+            # Try to find a span of free space that can fit the current file
+            for space_index in range(len(self._space_sizes)):
+                space_size = self._space_sizes[space_index]
+
+                # If the space can fit the file, move the file to this space
+                if space_size >= file_size:
+                    self._space_sizes[space_index] -= file_size
+                    self._space_sizes.insert(space_index, 0)
+
+                    combined_space_size = self._space_sizes[file_index - 1] + file_size + self._space_sizes[file_index]
+                    del self._space_sizes[file_index]
+                    self._space_sizes[file_index - 1] = combined_space_size
+
+                    file_id_order.remove(file_index)
+                    file_id_order.insert(space_index + 1, file_index)
+
+                    break
+
+        # Iterate through both lists to create tuples
+        for file_size, space_size, file_id in zip(self._file_sizes, self._space_sizes, file_id_order):
+            self._new_disk_layout.append((file_id, file_size))  # Add file
+            self._new_disk_layout.append((0, space_size))  # Add space with ID 0
 
 
 def main():
@@ -102,15 +141,17 @@ def main():
 
 
 class TestAdventOfCode(unittest.TestCase):
+    PUZZLE_INPUT = textwrap.dedent("""
+        2333133121414131402
+    """).strip()
+
     def test_part_one(self):
-        puzzle_input = textwrap.dedent("""
-            2333133121414131402
-        """).strip()
         expected_output = 1928
-        self.assertEqual(expected_output, solve_part_one(puzzle_input))
+        self.assertEqual(expected_output, solve_part_one(self.PUZZLE_INPUT))
 
     def test_part_two(self):
-        pass
+        expected_output = 2858
+        self.assertEqual(expected_output, solve_part_two(self.PUZZLE_INPUT))
 
 
 if __name__ == "__main__":
