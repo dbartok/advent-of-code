5186514b4902a4c725de4317a76208bbc23490ca
dbartok <34172998+dbartok@users.noreply.github.com>
2024-12-13 19:39:28 +0000

    Add Day 12: Garden Groups (part 2)

--- py/advent_of_code_2024/day_12_garden_groups/day_12_garden_groups.py
+++ py/advent_of_code_2024/day_12_garden_groups/day_12_garden_groups.py
@@ -25,9 +25,9 @@ def solve_part_one(puzzle_input):
     :return: Solution for part one
     """
     grid = [list(row) for row in puzzle_input.splitlines()]
-    garden_map = GardenPlotMap(grid)
-    garden_map.identify_regions()
-    return garden_map.get_total_price()
+    planner = GardenPlotPlanner(grid)
+    planner.identify_regions()
+    return planner.get_total_price()
 
 
 def solve_part_two(puzzle_input):
@@ -37,10 +37,13 @@ def solve_part_two(puzzle_input):
     :param puzzle_input: The input data as string
     :return: Solution for part two
     """
-    pass
+    grid = [list(row) for row in puzzle_input.splitlines()]
+    planner = GardenPlotPlannerWithDiscount(grid)
+    planner.identify_regions()
+    return planner.get_total_price()
 
 
-class GardenPlotMap:
+class GardenPlotPlanner:
     DIRECTIONS = [Vector(0, -1), Vector(0, 1), Vector(-1, 0), Vector(1, 0)]  # Up, Down, Left, Right
 
     def __init__(self, grid):
@@ -65,16 +68,10 @@ class GardenPlotMap:
 
         for region_cells in self._regions:
             area = len(region_cells)
-            perimeter = self._calculate_perimeter(region_cells)
+            perimeter = self._get_perimeter(region_cells)
             total_price += area * perimeter
 
         return total_price
-
-    def _get_plant_at(self, position):
-        return self._grid[int(position.y)][int(position.x)]
-
-    def _is_in_bounds(self, position):
-        return 0 <= position.x < self._width and 0 <= position.y < self._height
 
     def _get_region_at(self, start):
         queue = deque()
@@ -101,16 +98,82 @@ class GardenPlotMap:
 
         return region
 
+    def _get_plant_at(self, position):
+        return self._grid[int(position.y)][int(position.x)]
+
     def _get_neighbor_positions(self, position):
         return [position + direction for direction in self.DIRECTIONS]
 
-    def _calculate_perimeter(self, region):
+    def _is_in_bounds(self, position):
+        return 0 <= position.x < self._width and 0 <= position.y < self._height
+
+    def _get_perimeter(self, region):
         return sum(
             1
             for position in region
             for neighbor_position in self._get_neighbor_positions(position)
             if not self._is_in_bounds(neighbor_position) or neighbor_position not in region
         )
+
+
+class GardenPlotPlannerWithDiscount(GardenPlotPlanner):
+    def get_total_price(self):
+        total_price = 0
+
+        for region_cells in self._regions:
+            area = len(region_cells)
+            num_sides = self._get_num_sides(region_cells)
+            total_price += area * num_sides
+
+        return total_price
+
+    def _get_num_sides(self, region):
+        num_sides = 0
+
+        for direction in self.DIRECTIONS:
+            edge_positions = self._get_edge_positions_in_direction(region, direction)
+            num_sides += self._get_num_islands(edge_positions)
+
+        return num_sides
+
+    def _get_edge_positions_in_direction(self, region, direction):
+        edge_positions = set()
+
+        for position in region:
+            neighbor_position = position + direction
+            if (not self._is_in_bounds(neighbor_position)
+                    or self._get_plant_at(neighbor_position) != self._get_plant_at(position)):
+                edge_positions.add(neighbor_position)
+
+        return edge_positions
+
+    def _get_num_islands(self, positions):
+        visited = set()
+        num_islands = 0
+
+        for start_position in positions:
+            if start_position not in visited:
+                self._flood(start_position, positions, visited)
+                num_islands += 1
+
+        return num_islands
+
+    def _flood(self, start_position, positions, visited):
+        queue = deque()
+        queue.append(start_position)
+
+        while queue:
+            current_position = queue.popleft()
+
+            valid_neighbor_positions = [
+                neighbor_position
+                for neighbor_position in self._get_neighbor_positions(current_position)
+                if neighbor_position in positions and neighbor_position not in visited
+            ]
+
+            for neighbor_position in valid_neighbor_positions:
+                visited.add(neighbor_position)
+                queue.append(neighbor_position)
 
 
 def main():
@@ -125,24 +188,26 @@ def main():
 
 
 class TestAdventOfCode(unittest.TestCase):
+    PUZZLE_INPUT = textwrap.dedent("""
+        RRRRIICCFF
+        RRRRIICCCF
+        VVRRRCCFFF
+        VVRCCCJFFF
+        VVVVCJJCFE
+        VVIVCCJJEE
+        VVIIICJJEE
+        MIIIIIJJEE
+        MIIISIJEEE
+        MMMISSJEEE
+    """).strip()
+
     def test_part_one(self):
-        puzzle_input = textwrap.dedent("""
-            RRRRIICCFF
-            RRRRIICCCF
-            VVRRRCCFFF
-            VVRCCCJFFF
-            VVVVCJJCFE
-            VVIVCCJJEE
-            VVIIICJJEE
-            MIIIIIJJEE
-            MIIISIJEEE
-            MMMISSJEEE
-        """).strip()
         expected_output = 1930
-        self.assertEqual(expected_output, solve_part_one(puzzle_input))
+        self.assertEqual(expected_output, solve_part_one(self.PUZZLE_INPUT))
 
     def test_part_two(self):
-        pass
+        expected_output = 1206
+        self.assertEqual(expected_output, solve_part_two(self.PUZZLE_INPUT))
 
 
 if __name__ == "__main__":
