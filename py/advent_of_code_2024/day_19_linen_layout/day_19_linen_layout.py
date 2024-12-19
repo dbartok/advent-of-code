7b82562f2e47bb9125a8fded4adc85ab6946357a
dbartok <34172998+dbartok@users.noreply.github.com>
2024-12-19 14:25:14 +0000

    Add Day 19: Linen Layout (part 2)

--- py/advent_of_code_2024/day_19_linen_layout/day_19_linen_layout.py
+++ py/advent_of_code_2024/day_19_linen_layout/day_19_linen_layout.py
@@ -21,7 +21,9 @@ def solve_part_two(puzzle_input):
     :param puzzle_input: The input data as string
     :return: Solution for part two
     """
-    pass
+    available_patterns, desired_designs = parse_input(puzzle_input)
+    designer = TowelDesigner(available_patterns, desired_designs)
+    return designer.count_total_ways_to_form_all_designs()
 
 
 def parse_input(input_data):
@@ -37,22 +39,24 @@ class TowelDesigner:
         self._desired_designs = desired_designs
 
     def count_possible_designs(self):
-        return sum(1 for design in self._desired_designs if self._can_form_design(design))
+        return sum(1 for design in self._desired_designs if self._count_ways_to_form_design(design) > 0)
 
-    def _can_form_design(self, design):
-        # Dynamic Programming approach to check if the design can be formed
+    def count_total_ways_to_form_all_designs(self):
+        return sum(self._count_ways_to_form_design(design) for design in self._desired_designs)
+
+    def _count_ways_to_form_design(self, design):
+        # Dynamic Programming approach to count how many ways the design can be formed
         design_length = len(design)
-        can_form_up_to = [False] * (design_length + 1)  # can_form_up_to[i] is True if design[:i] can be formed
-        can_form_up_to[0] = True  # Base case: empty design can always be formed
+        ways = [0] * (design_length + 1)  # ways[i] is the number of ways to form design[:i]
+        ways[0] = 1  # Base case: empty design can always be formed in one way
 
         # Iterate over each position in the design
         for end_index in range(1, design_length + 1):
             for start_index in range(end_index):
-                if can_form_up_to[start_index] and design[start_index:end_index] in self._available_patterns:
-                    can_form_up_to[end_index] = True
-                    break
+                if ways[start_index] > 0 and design[start_index:end_index] in self._available_patterns:
+                    ways[end_index] += ways[start_index]
 
-        return can_form_up_to[design_length]  # Whether the entire design can be formed
+        return ways[design_length]  # The number of ways to form the entire design
 
 
 def main():
@@ -67,24 +71,26 @@ def main():
 
 
 class TestAdventOfCode(unittest.TestCase):
-    def test_part_one(self):
-        puzzle_input = textwrap.dedent("""
-            r, wr, b, g, bwu, rb, gb, br
+    PUZZLE_INPUT = textwrap.dedent("""
+        r, wr, b, g, bwu, rb, gb, br
 
-            brwrr
-            bggr
-            gbbr
-            rrbgbr
-            ubwu
-            bwurrg
-            brgr
-            bbrgwb
-        """).strip()
+        brwrr
+        bggr
+        gbbr
+        rrbgbr
+        ubwu
+        bwurrg
+        brgr
+        bbrgwb
+    """).strip()
+
+    def test_part_one(self):
         expected_output = 6
-        self.assertEqual(expected_output, solve_part_one(puzzle_input))
+        self.assertEqual(expected_output, solve_part_one(self.PUZZLE_INPUT))
 
     def test_part_two(self):
-        pass
+        expected_output = 16
+        self.assertEqual(expected_output, solve_part_two(self.PUZZLE_INPUT))
 
 
 if __name__ == "__main__":
