901b5eb67b56f1714d9dc9c21a23d307d46ff6cc
dbartok <34172998+dbartok@users.noreply.github.com>
2024-12-21 20:43:12 +0000

    Add Day 21: Keypad Conundrum (part 2)

--- py/advent_of_code_2024/day_21_keypad_conundrum/day_21_keypad_conundrum.py
+++ py/advent_of_code_2024/day_21_keypad_conundrum/day_21_keypad_conundrum.py
@@ -3,7 +3,8 @@ import unittest
 from functools import cache
 from pygame.math import Vector2 as Vector
 
-DEPTH = 2
+NUM_ROBOTS_PART_ONE = 2
+NUM_ROBOTS_PART_TWO = 25
 
 
 def solve_part_one(puzzle_input):
@@ -13,12 +14,7 @@ def solve_part_one(puzzle_input):
     :param puzzle_input: The input data as string
     :return: Solution for part one
     """
-
-    codes = puzzle_input.splitlines()
-    return sum(
-        KeypadSolver(code, DEPTH).calculate_min_length() * int(code[:3])
-        for code in codes
-    )
+    return solve_for_number_of_robots(puzzle_input, NUM_ROBOTS_PART_ONE)
 
 
 def solve_part_two(puzzle_input):
@@ -28,13 +24,21 @@ def solve_part_two(puzzle_input):
     :param puzzle_input: The input data as string
     :return: Solution for part two
     """
-    pass
+    return solve_for_number_of_robots(puzzle_input, NUM_ROBOTS_PART_TWO)
+
+
+def solve_for_number_of_robots(puzzle_input, num_robots):
+    codes = puzzle_input.splitlines()
+    return sum(
+        KeypadSolver(code, num_robots).calculate_min_length() * int(code[:3])
+        for code in codes
+    )
 
 
 class KeypadSolver:
-    def __init__(self, code, robot_depth):
+    def __init__(self, code, num_robots):
         self.code = code
-        self.robot_depth = robot_depth
+        self.num_robots = num_robots
 
         """
         +---+---+---+
@@ -84,27 +88,27 @@ class KeypadSolver:
         }
 
     def calculate_min_length(self):
-        return self.calculate_min_length_recursive(self.code, 0)
+        return self._calculate_min_length_recursive(self.code, 0)
 
     @cache
-    def calculate_min_length_recursive(self, desired_output, current_depth):
-        char_to_position = self._get_char_to_position(current_depth)
+    def _calculate_min_length_recursive(self, desired_output, current_robot):
+        char_to_position = self._get_char_to_position(current_robot)
         current_position = char_to_position["A"]
         total_minimum_length = 0
 
         for char in desired_output:
             next_position = char_to_position[char]
-            valid_paths = self.get_valid_paths(
+            valid_paths = self._get_valid_paths(
                 current_position, next_position, char_to_position
             )
 
-            # This is the "human" stage, who just types the sequence
-            if current_depth == self.robot_depth:
+            # We are past the number of robots (current_robot starts from zero), so this is the "human" stage, who just types out the sequence
+            if current_robot == self.num_robots:
                 total_minimum_length += len(valid_paths[0])
             # This is a "robot" stage, let's find the best possible length
             else:
                 total_minimum_length += min(
-                    self.calculate_min_length_recursive(move, current_depth + 1)
+                    self._calculate_min_length_recursive(move, current_robot + 1)
                     for move in valid_paths
                 )
             current_position = next_position
@@ -112,12 +116,13 @@ class KeypadSolver:
         return total_minimum_length
 
     def _get_char_to_position(self, current_depth):
+        # The first pad (depth 0) is a numpad, rest are directional pads
         if current_depth == 0:
             return self.NUMERIC_PAD_POSITIONS
 
         return self.DIRECTIONAL_PAD_POSITIONS
 
-    def get_valid_paths(self, start, end, char_to_position):
+    def _get_valid_paths(self, start, end, char_to_position):
         delta = end - start
         delta_x, delta_y = int(delta.x), int(delta.y)
 
@@ -150,21 +155,23 @@ def main():
 
 
 class TestAdventOfCode(unittest.TestCase):
-    def test_part_one(self):
-        puzzle_input = textwrap.dedent(
-            """
-            029A
-            980A
-            179A
-            456A
-            379A
+    PUZZLE_INPUT = textwrap.dedent(
         """
-        ).strip()
+        029A
+        980A
+        179A
+        456A
+        379A
+    """
+    ).strip()
+
+    def test_part_one(self):
         expected_output = 126384
-        self.assertEqual(expected_output, solve_part_one(puzzle_input))
+        self.assertEqual(expected_output, solve_part_one(self.PUZZLE_INPUT))
 
     def test_part_two(self):
-        pass
+        expected_output = 154115708116294
+        self.assertEqual(expected_output, solve_part_two(self.PUZZLE_INPUT))
 
 
 if __name__ == "__main__":
