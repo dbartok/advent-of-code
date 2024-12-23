7badb3ee3015a83fbd81731948b3741b43e35e78
dbartok <34172998+dbartok@users.noreply.github.com>
2024-12-23 13:16:35 +0000

    Add Day 23: LAN Party (part 2)

--- py/advent_of_code_2024/day_23_lan_party/day_23_lan_party.py
+++ py/advent_of_code_2024/day_23_lan_party/day_23_lan_party.py
@@ -23,7 +23,10 @@ def solve_part_two(puzzle_input):
     :param puzzle_input: The input data as string
     :return: Solution for part two
     """
-    pass
+    connections = parse_input(puzzle_input)
+    network = NetworkMap(connections)
+    network.find_largest_clique()
+    return network.get_largest_clique_password()
 
 
 def parse_input(puzzle_input):
@@ -34,6 +37,25 @@ class NetworkMap:
     def __init__(self, connections):
         self._node_to_neighbor_nodes = defaultdict(set)
         self._build_graph(connections)
+
+        self._largest_clique = []
+        self._visited = set()
+
+    def get_count_of_triad_sets_including_chief_historian(self):
+        return sum(
+            1
+            for triad in itertools.combinations(self._node_to_neighbor_nodes.keys(), 3)
+            if self._is_triad_connected(triad)
+            and any(computer.startswith("t") for computer in triad)
+        )
+
+    def find_largest_clique(self):
+        self._largest_clique = self._find_largest_clique_backtracking(
+            [], self._node_to_neighbor_nodes.keys()
+        )
+
+    def get_largest_clique_password(self):
+        return self._get_clique_password(self._largest_clique)
 
     def _build_graph(self, connections):
         for computer1, computer2 in connections:
@@ -48,14 +70,35 @@ class NetworkMap:
             and computer3 in self._node_to_neighbor_nodes[computer2]
         )
 
-    def get_count_of_triad_sets_including_chief_historian(self):
-        # Use list comprehension to count triads with at least one computer starting with 't'
-        return sum(
-            1
-            for triad in itertools.combinations(self._node_to_neighbor_nodes.keys(), 3)
-            if self._is_triad_connected(triad)
-            and any(computer.startswith("t") for computer in triad)
-        )
+    def _find_largest_clique_backtracking(
+        self, current_clique, neighbors_of_current_clique
+    ):
+        largest_clique = current_clique
+
+        for new_node_in_clique in neighbors_of_current_clique:
+            new_clique = current_clique + [new_node_in_clique]
+
+            if self._get_clique_password(new_clique) in self._visited:
+                continue
+            self._visited.add(self._get_clique_password(new_clique))
+
+            neighbors_of_new_clique = [
+                node
+                for node in neighbors_of_current_clique
+                if node != new_node_in_clique
+                and node in self._node_to_neighbor_nodes[new_node_in_clique]
+            ]
+            largest_clique_including_new_node = self._find_largest_clique_backtracking(
+                new_clique, neighbors_of_new_clique
+            )
+
+            if len(largest_clique_including_new_node) > len(largest_clique):
+                largest_clique = largest_clique_including_new_node
+
+        return largest_clique
+
+    def _get_clique_password(self, clique):
+        return ",".join(sorted(clique))
 
 
 def main():
@@ -70,48 +113,50 @@ def main():
 
 
 class TestAdventOfCode(unittest.TestCase):
-    def test_part_one(self):
-        puzzle_input = textwrap.dedent(
-            """
-            kh-tc
-            qp-kh
-            de-cg
-            ka-co
-            yn-aq
-            qp-ub
-            cg-tb
-            vc-aq
-            tb-ka
-            wh-tc
-            yn-cg
-            kh-ub
-            ta-co
-            de-co
-            tc-td
-            tb-wq
-            wh-td
-            ta-ka
-            td-qp
-            aq-cg
-            wq-ub
-            ub-vc
-            de-ta
-            wq-aq
-            wq-vc
-            wh-yn
-            ka-de
-            kh-ta
-            co-tc
-            wh-qp
-            tb-vc
-            td-yn
+    PUZZLE_INPUT = textwrap.dedent(
         """
-        ).strip()
+        kh-tc
+        qp-kh
+        de-cg
+        ka-co
+        yn-aq
+        qp-ub
+        cg-tb
+        vc-aq
+        tb-ka
+        wh-tc
+        yn-cg
+        kh-ub
+        ta-co
+        de-co
+        tc-td
+        tb-wq
+        wh-td
+        ta-ka
+        td-qp
+        aq-cg
+        wq-ub
+        ub-vc
+        de-ta
+        wq-aq
+        wq-vc
+        wh-yn
+        ka-de
+        kh-ta
+        co-tc
+        wh-qp
+        tb-vc
+        td-yn
+    """
+    ).strip()
+
+    def test_part_one(self):
         expected_output = 7
-        self.assertEqual(expected_output, solve_part_one(puzzle_input))
+        self.assertEqual(expected_output, solve_part_one(self.PUZZLE_INPUT))
 
     def test_part_two(self):
-        pass
+        expected_output = "co,de,ka,ta"
+        self.assertEqual(expected_output, solve_part_two(self.PUZZLE_INPUT))
 
 
 if __name__ == "__main__":
