c4a73b7264635e4857679a10c8defb7ad49b4280
dbartok <34172998+dbartok@users.noreply.github.com>
2025-12-11 21:07:38 +0000

    Add Day 11: Reactor (part 1)

---
+++ py/advent_of_code_2025/day_11_reactor/day_11_reactor.py
@@ -0,0 +1,69 @@
+import textwrap
+import unittest
+import networkx as nx
+
+
+def solve_part_one(puzzle_input):
+    """
+    Solve part one of the Advent of Code puzzle.
+
+    :param puzzle_input: The input data as string
+    :return: Solution for part one
+    """
+    G = nx.DiGraph()
+    for line in puzzle_input.splitlines():
+        src, rest = line.split(":")
+        src = src.strip()
+        targets = rest.strip().split()
+        for t in targets:
+            G.add_edge(src, t)
+    return len(list(nx.all_simple_paths(G, "you", "out")))
+
+
+def solve_part_two(puzzle_input):
+    """
+    Solve part two of the Advent of Code puzzle.
+
+    :param puzzle_input: The input data as string
+    :return: Solution for part two
+    """
+    pass
+
+
+def main():
+    with open("./input.txt") as f:
+        puzzle_input = f.read().strip()
+
+    part_one_result = solve_part_one(puzzle_input)
+    print(f"Part One: {part_one_result}")
+
+    part_two_result = solve_part_two(puzzle_input)
+    print(f"Part Two: {part_two_result}")
+
+
+class TestAdventOfCode(unittest.TestCase):
+    def test_part_one(self):
+        puzzle_input = textwrap.dedent(
+            """
+            aaa: you hhh
+            you: bbb ccc
+            bbb: ddd eee
+            ccc: ddd eee fff
+            ddd: ggg
+            eee: out
+            fff: out
+            ggg: out
+            hhh: ccc fff iii
+            iii: out
+        """
+        ).strip()
+        expected_output = 5
+        self.assertEqual(expected_output, solve_part_one(puzzle_input))
+
+    def test_part_two(self):
+        pass
+
+
+if __name__ == "__main__":
+    # unittest.main() # Uncomment to run unit tests
+    main()
