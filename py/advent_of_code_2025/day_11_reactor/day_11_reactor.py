310307ca4c26910ac0f60933c8ad5d368290eb1d
dbartok <34172998+dbartok@users.noreply.github.com>
2025-12-11 21:13:51 +0000

    Add Day 11: Reactor (part 2)

--- py/advent_of_code_2025/day_11_reactor/day_11_reactor.py
+++ py/advent_of_code_2025/day_11_reactor/day_11_reactor.py
@@ -10,13 +10,7 @@ def solve_part_one(puzzle_input):
     :param puzzle_input: The input data as string
     :return: Solution for part one
     """
-    G = nx.DiGraph()
-    for line in puzzle_input.splitlines():
-        src, rest = line.split(":")
-        src = src.strip()
-        targets = rest.strip().split()
-        for t in targets:
-            G.add_edge(src, t)
+    G = build_graph(puzzle_input)
     return len(list(nx.all_simple_paths(G, "you", "out")))
 
 
@@ -27,7 +21,46 @@ def solve_part_two(puzzle_input):
     :param puzzle_input: The input data as string
     :return: Solution for part two
     """
-    pass
+    G = build_graph(puzzle_input)
+
+    # case A: svr -> dac -> fft -> out
+    a1 = count_paths_dag(G, "svr", "dac")
+    a2 = count_paths_dag(G, "dac", "fft")
+    a3 = count_paths_dag(G, "fft", "out")
+    case_a = a1 * a2 * a3
+
+    # case B: svr -> fft -> dac -> out
+    b1 = count_paths_dag(G, "svr", "fft")
+    b2 = count_paths_dag(G, "fft", "dac")
+    b3 = count_paths_dag(G, "dac", "out")
+    case_b = b1 * b2 * b3
+
+    return case_a + case_b
+
+
+def build_graph(puzzle_input):
+    G = nx.DiGraph()
+    for line in puzzle_input.splitlines():
+        src, rest = line.split(":")
+        src = src.strip()
+        targets = rest.strip().split()
+        for t in targets:
+            G.add_edge(src, t)
+    return G
+
+
+def count_paths_dag(G, start, end, memo=None):
+    if memo is None:
+        memo = {}
+    if start == end:
+        return 1
+    if start in memo:
+        return memo[start]
+    total = 0
+    for neighbor in G.successors(start):
+        total += count_paths_dag(G, neighbor, end, memo)
+    memo[start] = total
+    return total
 
 
 def main():
@@ -61,7 +94,25 @@ class TestAdventOfCode(unittest.TestCase):
         self.assertEqual(expected_output, solve_part_one(puzzle_input))
 
     def test_part_two(self):
-        pass
+        puzzle_input = textwrap.dedent(
+            """
+            svr: aaa bbb
+            aaa: fft
+            fft: ccc
+            bbb: tty
+            tty: ccc
+            ccc: ddd eee
+            ddd: hub
+            hub: fff
+            eee: dac
+            dac: fff
+            fff: ggg hhh
+            ggg: out
+            hhh: out
+        """
+        ).strip()
+        expected_output = 2
+        self.assertEqual(expected_output, solve_part_two(puzzle_input))
 
 
 if __name__ == "__main__":
